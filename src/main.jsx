3de46b60f40cb87081b73fb3f3cfe3d1a37110a1
PawanAK <pawanajjark@gmail.com>
2024-06-18 18:56:37 +0530

    fix 2 route

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { ArweaveWalletKit } from '@arweave-wallet-kit/react'
-import ArConnectStrategy from '@arweave-wallet-kit/arconnect-strategy'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ArweaveWalletKit } from "@arweave-wallet-kit/react";
+import ArConnectStrategy from "@arweave-wallet-kit/arconnect-strategy";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Create from "./pages/Create.jsx";
+import View from "./pages/View.jsx";
+import ViewPost from "./pages/ViewPost.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/view",
+    element: <View />,
+  },
+  {
+    path: "/view/:postId",
+    element: <ViewPost />,
+  },
+  {
+    path: "/create",
+    element: <Create />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ArweaveWalletKit
       config={{
         permissions: [
-          'ACCESS_ADDRESS',
-          'ACCESS_ALL_ADDRESSES',
-          'SIGN_TRANSACTION',
-          'DISPATCH',
+          "ACCESS_ADDRESS",
+          "ACCESS_ALL_ADDRESSES",
+          "SIGN_TRANSACTION",
+          "DISPATCH",
         ],
         ensurePermissions: true,
         strategies: [new ArConnectStrategy()],
       }}
     >
-      <App />
+      <RouterProvider router={router} />
     </ArweaveWalletKit>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
